06a919de9e20999ad43baf9dbf5c14ea5dc457d1
dir-FT24kotabekeigo <keigo.kotabe.c443@dirbato.co.jp>
2024-04-09 11:15:47 +0900

    4/9

---
+++ Sample/src/sample/ArraySample.java
@@ -0,0 +1,20 @@
+package sample;
+
+import java.security.PublicKey;
+
+public class ArraySample {
+
+    public static void main(String[] args) {
+        var numbers = new int[5];
+        
+        for (var i = 0; i < numbers.length; i++) {
+            numbers[i] = i * 2;
+        }
+        
+        for (var i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+
+    }
+
+}
